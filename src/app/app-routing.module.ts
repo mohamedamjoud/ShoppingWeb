30d2ddf5e704e0f4179b9f3161e97649c66af148
mohamedamjoud <mohamedamjoud@outlook.com>
2020-06-06 01:00:10 +0100

    add login component Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
 import { ManageProductsComponent } from './manage-products/manage-products.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
@@ -27,6 +28,10 @@ const routes: Routes = [
   {
     path: 'cart',
     component: ShoppingCartComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
 
 ];
